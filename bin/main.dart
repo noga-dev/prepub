495fc8a77f087211a074d5be4d023f24268d9e45
Agon Noga <github@noga.dev>
2022-10-22 15:47:57 +0300

    wip

--- bin/main.dart
+++ bin/main.dart
@@ -8,7 +8,8 @@ void main(List<String> arguments) {
   final files = options.files;
 
   if (files.every((e) => RegExp(e).hasMatch(r'*.(md|mdx)$'))) {
-    log('Only markdown files were modified.');
+    log('[Log] Only markdown files were modified.');
+    print('[Print] Only markdown files were modified.');
     exitCode = 0;
   }
 
@@ -16,7 +17,7 @@ void main(List<String> arguments) {
   log('logStart');
   print('::set-output name=testval::111');
   log('::set-output name=testval::222');
-  print('Hello world: ${files.length}!');
+  print('Found ${files.length} changed files.');
   log('logEnd');
   print('printEnd');
 }
